83a17d327fba2fd2b35d4d28cafe99a5a2e8130c
Rakan <Charmerrk@gmail.com>
2022-08-31 14:10:18 +0800

    添加项目文件。

---
+++ Dao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    internal class Dao
+    {
+
+
+    }
+}
